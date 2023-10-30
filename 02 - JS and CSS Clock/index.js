de637de009789ce92a04f73b5fb5521badeb84af
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-10-30 10:55:57 +0300

    Log the current time

--- 02 - JS and CSS Clock/index.js
+++ 02 - JS and CSS Clock/index.js
@@ -16,6 +16,8 @@ function setDate(){
     const hours = now.getHours();
     const hoursDegrees = ((hours / 12) * 360) + 90;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+    console.log(hours+":"+minutes+":"+seconds)
    
 }
 
