0b85ed12124591a58e5b4f8304dda204beb52282
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-10-30 11:44:46 +0300

    Change CSS variables using JS

---
+++ 03 - CSS Variables/index.js
@@ -0,0 +1,11 @@
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate(){
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value+suffix);
+}
+
+inputs.forEach(input=>{
+    input.addEventListener('change', handleUpdate);
+    input.addEventListener('mousemove', handleUpdate);
+})
