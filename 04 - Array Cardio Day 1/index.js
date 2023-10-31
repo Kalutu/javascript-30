ee08a70ae29113aa4c5e523f84fb44ff5eb9d017
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-10-31 11:57:48 +0300

    Set up

--- 04 - Array Cardio Day 1/index.js
+++ 04 - Array Cardio Day 1/index.js
@@ -28,12 +28,18 @@
       
       // Array.prototype.filter()
       // 1. Filter the list of inventors for those who were born in the 1500's
-  
+      const fifteen = inventors.filter(inventor=>inventor.year>=1500 && inventor.year<1600)
+      console.table(fifteen);
+
       // Array.prototype.map()
       // 2. Give us an array of the inventors first and last names
+      const Fullnames = inventors.map(inventor => inventor.first +" "+ inventor.last)
+      console.table(Fullnames)
   
       // Array.prototype.sort()
       // 3. Sort the inventors by birthdate, oldest to youngest
+      const sorted = inventors.sort((a,b)=>a.year-b.year)
+      console.table(sorted)
   
       // Array.prototype.reduce()
       // 4. How many years did all the inventors live all together?
