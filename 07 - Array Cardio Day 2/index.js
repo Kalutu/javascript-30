dcd47ad71274c967e36238ba6d4d90fa29b3c9dc
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-12-02 13:12:45 +0300

    Array methods

--- 07 - Array Cardio Day 2/index.js
+++ 07 - Array Cardio Day 2/index.js
@@ -15,12 +15,35 @@
 
   // Some and Every Checks
   // Array.prototype.some() // is at least one person 19 or older?
+  const isAdult = people.some(person=>{
+    const currentYear = new Date().getFullYear()
+    const age = currentYear - person.year
+    return age >= 19
+  })
+  console.log({isAdult})
+  
   // Array.prototype.every() // is everyone 19 or older?
+  
+  const isAllAdults = people.every(person=>{
+    const currentYear = new Date().getFullYear()
+    const age = currentYear - person.year
+    return age >= 19
+  })
+  console.log({isAllAdults})
 
   // Array.prototype.find()
   // Find is like filter, but instead returns just the one you are looking for
   // find the comment with the ID of 823423
+  const comment = comments.find(comment=>comment.id == 823423)
+  console.log(comment)
 
   // Array.prototype.findIndex()
   // Find the comment with this ID
   // delete the comment with the ID of 823423
+  const index = comments.findIndex(comment=>comment.id == 823423)
+  console.log(index)
+  console.table(comments)
+  
+  comments.splice(index, 1)//delete the selected comment
+  console.table(comments)
+
