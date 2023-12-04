47ac4f15e0a8e1bad95d2fed9fcabc06c0c2ebab
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-12-04 11:52:53 +0300

    Konami code

--- 12 - Key Sequence Detection/index.js
+++ 12 - Key Sequence Detection/index.js
@@ -4,5 +4,10 @@ const secretCode = 'kalutu'
 window.addEventListener('keyup',(e)=>{
     console.log(e.key)
     pressed.push(e.key)
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length)
+    if(pressed.join('').includes(secretCode)){
+        console.log('Did it')
+        cornify_add();
+    }
     console.log(pressed)
 })
