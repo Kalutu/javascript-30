da6de31cf7fe82df072d746f5b59eee6e7ae5cee
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-12-11 10:27:31 +0300

    click and drag

--- 27 - Click and Drag/index.js
+++ 27 - Click and Drag/index.js
@@ -3,20 +3,27 @@ let isDown = false
 let startX;
 let scrollLeft;
 
-slider.addEventListener('mousedown', ()=>{
+slider.addEventListener('mousedown', (e)=>{
     isDown = true
+    slider.classList.add('active')
+    startX = e.pageX - slider.offsetLeft
+    scrollLeft = slider.scrollLeft 
 })
 
 slider.addEventListener('mouseleave', ()=>{
     isDown = false
+    slider.classList.remove('active')
 })
 
 slider.addEventListener('mouseup', ()=>{
     isDown = false   
+    slider.classList.remove('active')
 })
 
-slider.addEventListener('mousemove', ()=>{
+slider.addEventListener('mousemove', (e)=>{
     if(!isDown) return
-    console.log(isDown)
-    console.log('do work')
+    e.preventDefault()
+    const x = e.pageX - slider.offsetLeft
+    const walk = (x - startX) * 3
+    slider.scrollLeft = scrollLeft - walk
 })
