2bef6395ba9ace4775819c6878139110c920ab39
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-12-11 11:48:33 +0300

    display end time

--- 29 - Countdown Timer/index.js
+++ 29 - Countdown Timer/index.js
@@ -1,20 +1,26 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left')
+const endtime = document.querySelector('.display__end-time')
+const buttons = document.querySelectorAll('[data-time]')
 
 function timer(seconds){
+  clearInterval(countdown)
+
   const now = Date.now()
-  const then = now + (seconds * 1000)
+  const then = now + seconds * 1000
+
   displayTimeLeft(seconds)
+  displayEndTime(then)
 
   countdown = setInterval(()=>{
     const secondsLeft = Math.round((then - Date.now())/1000)
-
+    
     if(secondsLeft < 0){
-        clearInterval(countdown)
-        return
+      clearInterval(countdown)
+      return
     }
 
-    displayTimeLeft(seconds)
+    displayTimeLeft(secondsLeft)
 
   },1000)
 }
@@ -22,6 +28,29 @@ function timer(seconds){
 function displayTimeLeft(seconds){
     const minutes = Math.floor(seconds / 60)
     const remainderSeconds = seconds % 60
-    const display = `${minutes} : ${remainderSeconds}`
+    const display = `${minutes} : ${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`
+
+    document.title = display
     timerDisplay.textContent = display
 }
+
+function displayEndTime(timestamp){
+  const end = new Date(timestamp)
+  const hour = end.getHours()
+  const minutes = end.getMinutes()
+  endtime.textContent = `Be Back At ${hour}: ${minutes < 10 ? '0' : ''}${minutes}`
+}
+
+function startTimer(){
+  const seconds = parseInt(this.dataset.time)
+  timer(seconds)
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer))
+
+document.customForm.addEventListener('submit',function(e){
+  e.preventDefault()
+  const mins = this.minutes.value
+  timer(mins *60)
+  this.reset()
+})
